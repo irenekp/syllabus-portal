5ed851b702b7b53ac397b94e2b03aa9d073d5e2b
paplio <ani.12.28.98@gmail.com>
2020-11-08 13:47:33 +0530

    basic functionality for filelist, uploading redirects to filelist now, expanded form a lil by adding prof and code attributes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_table import Table,Col
 from flask import render_template, request
 #from werkzeug import secure_filename
 import database
@@ -22,25 +23,29 @@ def useruploading():
 def serveruploader():
 	if request.method == 'POST':
 		f = request.files['myfile']
+		course = request.form.get("course", None)
+		prof = request.form.get("prof", None)
+		print(course)
 		f.save(os.path.join("static/",f.filename))
-		database.db.Resources.insert_one({"course_code":"ABC","filepath":f.filename})
+		database.db.Resources.insert_one({"course_code":course, "prof":prof, "filepath":f.filename})
 		#f.save(f.filename)
-		return 'file uploaded successfully'
+		print("Uploaded files successfully")
+		return filelist()
 
 #ToDo: filelist: multiple file+properties to template
 @app.route('/filelist')
 def filelist():
-	cursor=database.db.Resources.find( { "course_code": "ABC" } )
-	op=""
+	cursor=database.db.Resources.find()
+	op=[]
 	for item in cursor:
-		print(item)
-	return "check commandline"
+		op.append(item)
+	return render_template("filelist.html", cursor=op)
 
 #display individual file, make it prettier, call it from onclick of 
 @app.route("/viewfile")
 def viewfile():
 	#read filepath from db into filepath variable
-	filepath='Entry1.pdf'
+	filepath='MC Paper.pdf'
 	return render_template("showfile.html",filepath=filepath)
 """
 @app.route("/test")
